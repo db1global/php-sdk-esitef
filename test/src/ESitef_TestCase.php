9a2b6cb2554ee858815d59a7ba93236a5a156e3f
Rafael Ito <rafaelcarvalhoito@gmail.com>
2019-09-11 16:48:51 -0300

    Initial commit

---
+++ test/src/ESitef_TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Test\DB1\Acquirer\ESitef;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class ESitef_TestCase extends TestCase
+{
+
+}
---
+++ test/src/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Test\DB1\Acquirer\ESitef\ESitef_TestCase;
+
+class ExampleTest extends ESitef_TestCase
+{
+	/**
+	 * A basic test example.
+	 *
+	 * @return void
+	 */
+	public function testExample()
+	{
+		$this->assertTrue(true);
+	}
+}
