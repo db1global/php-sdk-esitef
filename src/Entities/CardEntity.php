c0199bcbc2119d64d02f277124e85f7d1344f99b
Cezar Aluisio Pavelski <cezarpavelski@gmail.com>
2019-09-11 18:44:43 -0300

    Created CardEntity

---
+++ src/Entities/CardEntity.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace DB1\Acquirer\ESitef\Entities;
+
+use DB1\Acquirer\ESitef\Exceptions\MutateInstanceException;
+
+class CardEntity
+{
+	/** @var string */
+	private $card_number;
+
+	/** @var string */
+	private $card_expiry_date;
+
+	/** @var string */
+	private $card_cvv;
+
+	/** @var string */
+	private $nit;
+
+	private function __construct(){}
+
+	public static function builder(): CardEntityBuilder
+	{
+		$instance = new self();
+		$propertySetter = function ($name, $value) use ($instance) {
+			$instance->{$name} = $value;
+		};
+
+		return new CardEntityBuilder($instance, $propertySetter);
+	}
+
+	public function getCardNumber(): string
+	{
+		return $this->card_number;
+	}
+
+	public function getCardExpiryDate(): string
+	{
+		return $this->card_expiry_date;
+	}
+
+	public function getCardCvv(): string
+	{
+		return $this->card_cvv;
+	}
+
+	public function getNit(): string
+	{
+		return $this->nit;
+	}
+}
+
+class CardEntityBuilder
+{
+	/** @var string */
+	private $card_number;
+
+	/** @var string */
+	private $card_expiry_date;
+
+	/** @var string */
+	private $card_cvv;
+
+	/** @var string */
+	private $nit;
+
+	public function __construct(CardEntity $instance, callable $propertySetter)
+	{
+		$this->instance = $instance;
+		$this->propertySetter = $propertySetter;
+	}
+
+	/**
+	 * @return CardEntity
+	 * @throws MutateInstanceException
+	 */
+	public function build(): CardEntity
+	{
+		$this->validateBuild();
+		$this->setProperty('card_number', $this->card_number);
+		$this->setProperty('card_expiry_date', $this->card_expiry_date);
+		$this->setProperty('card_cvv', $this->card_cvv);
+		$this->setProperty('nit', $this->nit);
+		unset($this->propertySetter);
+		return $this->instance;
+	}
+
+	/**
+	 * @param string $name
+	 * @param $value
+	 * @return CardEntityBuilder
+	 * @throws MutateInstanceException
+	 */
+	private function setProperty(string $name, $value): self
+	{
+		if (!isset($this->propertySetter)) {
+			throw new MutateInstanceException();
+		}
+
+		$propertySetter = $this->propertySetter;
+		$propertySetter($name, $value);
+
+		return $this;
+	}
+
+	public function with_card_number(string $card_number): self
+	{
+		$this->card_number = $card_number;
+		return $this;
+	}
+
+	public function with_card_expiry_date(string $card_expiry_date): self
+	{
+		$this->card_expiry_date = $card_expiry_date;
+		return $this;
+	}
+
+	public function with_card_cvv(string $card_cvv): self
+	{
+		$this->card_cvv = $card_cvv;
+		return $this;
+	}
+
+	public function with_nit(string $nit): self
+	{
+		$this->nit = $nit;
+		return $this;
+	}
+
+	private function validateBuild(): void {
+		if(!$this->card_number){
+			throw new \InvalidArgumentException('Parameter card_number cannot be empty');
+		}
+		if(!$this->card_expiry_date){
+			throw new \InvalidArgumentException('Parameter card_expiry_date cannot be empty');
+		}
+		if(!$this->card_cvv){
+			throw new \InvalidArgumentException('Parameter card_cvv cannot be empty');
+		}
+		if(!$this->nit){
+			throw new \InvalidArgumentException('Parameter nit cannot be empty');
+		}
+	}
+}
---
+++ test/src/Entities/CardEntityTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Test\Acquirer\ESitef\Entities;
+
+use DB1\Acquirer\ESitef\Entities\CardEntity;
+use PHPUnit\Framework\TestCase;
+
+class CardEntityTest extends TestCase {
+
+	public function test_CardEntityBuilder_ShouldBuild(): void
+	{
+		$card = CardEntity::builder()
+			->with_card_number($card_number = '5555555555555555')
+			->with_card_expiry_date($card_expiry_date = '1222')
+			->with_card_cvv($card_cvv = '123')
+			->with_nit($nit = 'QWEQWEXXX00000342D22SDFAAZD')
+			->build();
+
+		$this->assertEquals($card_number, $card->getCardNumber());
+		$this->assertEquals($card_expiry_date, $card->getCardExpiryDate());
+		$this->assertEquals($card_cvv, $card->getCardCvv());
+		$this->assertEquals($nit, $card->getNit());
+	}
+
+	public function test_CardEntityBuilder_WithWrongData_ShouldThrowException(): void
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$this->expectExceptionMessage('Parameter card_number cannot be empty');
+		CardEntity::builder()->build();
+	}
+
+	/** @dataProvider invalidDataProvider */
+	public function test_CardEntityBuilderWithInvalidData_ShouldThrowException(
+		string $card_number,
+		string $card_expiry_date,
+		string $card_cvv,
+		string $nit,
+		string $expected_message
+	): void
+	{
+		self::expectException(\InvalidArgumentException::class);
+		self::expectExceptionMessage($expected_message);
+
+		CardEntity::builder()
+			->with_card_number($card_number)
+			->with_card_expiry_date($card_expiry_date)
+			->with_card_cvv($card_cvv)
+			->with_nit($nit)
+			->build();
+	}
+
+	public function invalidDataProvider(): array
+	{
+		return [
+			//$card_number     $card_expiry_date  $card_cvv   $nit              $expected_message
+			[''                , '1222'           , '123'     ,'QWEQWEXXX0000'  , 'Parameter card_number cannot be empty'],
+			['5555555555555555', ''               , '123'     ,'QWEQWEXXX0000'  , 'Parameter card_expiry_date cannot be empty'],
+			['5555555555555555', '1222'           , ''        ,'QWEQWEXXX0000'  , 'Parameter card_cvv cannot be empty'],
+			[''                , ''               , ''        ,'QWEQWEXXX0000'  , 'Parameter card_number cannot be empty'],
+		];
+	}
+}
